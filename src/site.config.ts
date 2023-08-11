1280354a6a92af6fdd6b486b68d7fe959b81da45
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-08-11 17:46:07 +0200

    feat: base layout and css theme

---
+++ src/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string;
+	title: string;
+	description: string;
+	lang: string;
+	ogLocale: string;
+	themeColorLight: string;
+	themeColorDark: string;
+}
+
+export const siteConfig: SiteConfig = {
+	author: "Sebastien LOVERGNE",
+	title: "Sebastien LOVERGNE",
+	description: "A site about me and my interests",
+	lang: "en-US",
+	ogLocale: "en_US",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
