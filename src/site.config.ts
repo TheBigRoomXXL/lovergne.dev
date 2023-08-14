2866fca6b85380e39ad5c24290fc0a22c3d7dc10
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-08-14 09:23:44 +0200

    refacto: improve header

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,3 +1,8 @@
+interface Link {
+	name : string;
+	url: string
+}
+
 interface SiteConfig {
 	author: string;
 	title: string;
@@ -5,6 +10,8 @@ interface SiteConfig {
 	lang: string;
 	ogLocale: string;
 	themeColor: string;
+	menuLinks: Link[];
+	socialLinks: Link[];
 }
 
 export const siteConfig: SiteConfig = {
@@ -14,4 +21,17 @@ export const siteConfig: SiteConfig = {
 	lang: "en-US",
 	ogLocale: "en_US",
 	themeColor: "#0ac8f5",
+	menuLinks: [
+		{name: "Home", url: "/"},
+		{name: "Projects", url: "/projects"},
+		{name: "Reading lists", url: "/readings"},
+		{name: "Resume", url: "/resume"},
+		{name: "RSS", url: "/rss.xml"},
+
+	],
+	socialLinks: [
+		{name: "github", url: "https://github.com/TheBigRoomXXL"},
+		{name: "itch.io", url: "https://tehbigroomxxl.itch.io/"},
+		{name: "email", url: "sebastien@lovergne.dev"},
+	],
 };
