f4b260a38d6f4a97c56d9e8dce233215ee368111
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-07-14 11:42:55 +0200

    feat: render archive in plain text format

---
+++ src/pages/archive/[slug].txt.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from 'astro';
+import { TXT_WIDTH, h1 } from "src/components.txt/formatter"
+import { Header } from "src/components.txt/Header.txt"
+import { Footer } from "src/components.txt/Footer";
+import { getCollection } from "astro:content";
+
+import { renderMarkdownToPlainTxt } from 'src/components.txt/tree-renderer';
+
+let notes = await getCollection("zettelkasten");
+
+export async function getStaticPaths() {
+    const note = await getCollection("zettelkasten");
+    return note.map((note) => ({
+        params: { slug: note.slug },
+        props: note,
+    }));
+}
+
+notes.sort((n1, n2) => {
+    if (n1.data.added_date > n2.data.added_date) return -1;
+    else if (n1.data.added_date < n2.data.added_date) return 1;
+    else return 0;
+});
+
+
+export const GET: APIRoute = ({ props }) => {
+    let result = Header()
+
+    let title = props.data.title
+    if (props.data.author) {
+        title += ` by ${props.data.author}`
+    }
+    if (props.data.published_date) {
+        title += ` (${props.data.published_date.getFullYear()})`
+    }
+    result += h1(title)
+
+    result += renderMarkdownToPlainTxt(props.body)
+
+    result += Footer()
+
+    return new Response(result)
+}
+
+
