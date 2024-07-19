eed99a7b86c189a327a2d43205d11ae2f14e5e4f
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-07-19 19:23:40 +0200

    misc: increase txt footer padding

--- src/components.txt/Footer.ts
+++ src/components.txt/Footer.ts
@@ -1,7 +1,7 @@
 import { center } from "./formatter"
 
 export function Footer(): string {
-    let result = "\n".repeat(13)
+    let result = "\n".repeat(20)
 
     result += center(
         "The bottom of every page is padded so readers can maintain a consistent eyeline\n"
