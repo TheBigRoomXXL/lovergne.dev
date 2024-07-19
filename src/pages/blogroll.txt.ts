7501d5615d74873966125f707b1da19e264c95e7
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-07-19 19:22:51 +0200

    feat: add txt version of blogroll page

---
+++ src/pages/blogroll.txt.ts
@@ -0,0 +1,31 @@
+import { h1, paragraph, bold } from "src/components.txt/formatter"
+import { Header } from "src/components.txt/Header.txt"
+import { Footer } from "src/components.txt/Footer";
+import { Card } from "src/components.txt/Card.txt";
+import { getCollection } from "astro:content";
+
+let blogroll = await getCollection("zettelkasten");
+blogroll = blogroll
+    .filter((n) => n.data.tags.includes("blog"))
+    .sort((b1, b2) => {
+        if (b2.data.pin) return 1;
+        if (b1.data.pin) return -1;
+        else return Math.random() >= 0.5 ? -1 : 1;
+    });
+
+const cards = blogroll.reduce((acc, blog) => {
+    return acc + Card(blog)
+}, "")
+
+export async function GET() {
+    return new Response(
+        Header() +
+        h1("Blogroll") +
+        paragraph(`A (partial) list of blogs I like and consistently read. You can follow all
+    of them through the magic of RSS.`) +
+        paragraph(`The list is sorted randomly, expect for my favorites wich are pinned.`) +
+        cards +
+        Footer()
+    )
+}
+
