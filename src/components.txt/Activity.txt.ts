9001e076ed648205e53176ccaaa501e74bf6ebc5
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-03-09 17:19:23 +0100

    feat: make \index.txt\ more mobile friendly

--- src/components.txt/Activity.txt.ts
+++ src/components.txt/Activity.txt.ts
@@ -7,9 +7,6 @@ let notes = await getCollection("zettelkasten");
 let longestSlug = 0
 
 notes.sort((n1, n2) => {
-    if (n1.slug.length > longestSlug) {
-        longestSlug = n1.slug.length
-    }
     if (n1.data.added_date > n2.data.added_date) return -1;
     else if (n1.data.added_date < n2.data.added_date) return 1;
     else return 0;
@@ -21,24 +18,17 @@ export function Activity(): string {
     let result = h2("Recent Activities")
     result += "To read an article go to https://lovergne.dev/archive/<SLUG>.txt\n\n"
 
-    const titleSize = TXT_WIDTH - 13 - longestSlug
-
-    result += "DATE         " + "TITLE" + " ".repeat(titleSize - 4) + "SLUG\n"
-    result += "─".repeat(TXT_WIDTH) + "\n"
+    const titleSize = TXT_WIDTH - 13
 
     notes.forEach((note) => {
-        result += formatDate(note.data.added_date) + "  "
-
-
+        // result += formatDate(note.data.added_date) + ": "
         let title = note.data.title
-        if (title.length < titleSize) {
-            title += " ".repeat(titleSize - title.length)
-        }
         if (title.length > titleSize) {
             title = title.slice(0, titleSize - 1) + "…"
         }
+        result += title + "\n"
 
-        result += `${title} ${note.slug}\n`
+        result += `  ↳ https://lovergne.dev/archive/${note.slug}\n\n`
     })
 
 
