a7c9b7f0a06decc1d3eeb91267c2adab74874bcd
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-04-29 14:25:33 +0200

    feat: add support for markdown footnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import footnote from 'markdown-it-footnote';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lovergne.dev",
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  markdown: {
+    extendMarkdownConfig(md) {
+      md.use(footnote);
+    },
+  },
 });
