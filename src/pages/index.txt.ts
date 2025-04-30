20677dc98c73520c56c28216e2b989f2f1501293
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-04-30 09:48:27 +0200

    content: update landing page presentation

--- src/pages/index.txt.ts
+++ src/pages/index.txt.ts
@@ -18,13 +18,12 @@ export async function GET() {
         h1("Hello World!") +
         paragraph(`Welcome! My name is Sebastien Lovergne. I'm an inveterate 
         tinkerer and I work as a software engineer.`) +
-        paragraph(`This website is a place where I can experiment and be 
-        creative. I mostly publish links to external resources that I have found
-        interesting or thought-provoking (and some fun stuff think of it as my
-        personal web archive. I might also write about some of my personal 
-        projects in the future.`) +
-        paragraph(`You can find me as @TheBigRoomXXL on github and itch.io
-        or contacts me by mail at seb.lovergne@gmail.com`) +
+        paragraph(`This website is a place where I can experiment and be creative. I write
+      about some of my personal projects when I think they are interesting
+      enough and I'm not too lazy to write. I also publish links to external
+      resources that I have found interesting or thought-provoking. Think of
+      it as my personal web archive.`) +
+        paragraph(`You can find me as @TheBigRoomXXL on github and itch.io`) +
         Activity() +
         Footer()
     )
