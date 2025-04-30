b1a24cd16f56875fbca7c0b693a0cbb32a4c51eb
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-04-30 15:51:31 +0200

    fix: outdated txt header

--- src/components.txt/Header.txt.ts
+++ src/components.txt/Header.txt.ts
@@ -1,10 +1,10 @@
 export function Header(): string {
     return String.raw`  _____ _ 
  / ____| |           Sebastien LOVERGNE
-| (___ | |           /projects.txt
- \___ \| |           /awesome.txt
- ____) | |____       /blogroll.txt
-|_____/|______|      /rss.xml
+| (___ | |           /blog.txt
+ \___ \| |           /projects.txt
+ ____) | |____       /awesome.txt
+|_____/|______|      /blogroll.txt
 
 
 `
