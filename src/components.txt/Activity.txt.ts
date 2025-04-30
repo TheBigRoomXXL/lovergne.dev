f58b291162c7209a591d6d64328f15fdf9225627
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-04-30 09:41:18 +0200

    feat: restructure "Recent Activities" in two sections

--- src/components.txt/Activity.txt.ts
+++ src/components.txt/Activity.txt.ts
@@ -4,22 +4,39 @@ import { formatDate } from "src/components/utils";
 
 
 let notes = await getCollection("zettelkasten");
-let longestSlug = 0
 
-notes.sort((n1, n2) => {
-    if (n1.data.added_date > n2.data.added_date) return -1;
-    else if (n1.data.added_date < n2.data.added_date) return 1;
-    else return 0;
-});
+const postAndProject = notes
+    .filter(
+        (n) => n.data.tags.includes("blog-post") || n.data.tags.includes("project"),
+    )
+    .sort((n1, n2) => {
+        if (n2.data.pin) return 1;
+        if (n1.data.pin) return -1;
+        else if (n1.data.added_date > n2.data.added_date) return -1;
+        else if (n1.data.added_date < n2.data.added_date) return 1;
+        else return 0;
+    });
 
+const others = notes
+    .filter(
+        (n) =>
+            !n.data.tags.includes("blog-post") && !n.data.tags.includes("project"),
+    )
+    .sort((n1, n2) => {
+        if (n2.data.pin) return 1;
+        if (n1.data.pin) return -1;
+        else if (n1.data.added_date > n2.data.added_date) return -1;
+        else if (n1.data.added_date < n2.data.added_date) return 1;
+        else return 0;
+    });
 
 
 export function Activity(): string {
-    let result = h2("Recent Activities")
+    let result = h2("Blog posts and projects")
+
     const titleSize = TXT_WIDTH - 13
 
-    notes.forEach((note) => {
-        // result += formatDate(note.data.added_date) + ": "
+    postAndProject.forEach((note) => {
         let title = note.data.title
         if (title.length > titleSize) {
             title = title.slice(0, titleSize - 1) + "…"
@@ -29,6 +46,16 @@ export function Activity(): string {
         result += ` ↳ https://lovergne.dev/archive/${note.slug}\n\n`
     })
 
+    result += h2('Awesome collection and blogroll')
+    others.forEach((note) => {
+        let title = note.data.title
+        if (title.length > titleSize) {
+            title = title.slice(0, titleSize - 1) + "…"
+        }
+        result += title + "\n"
+
+        result += ` ↳ https://lovergne.dev/archive/${note.slug}\n\n`
+    })
 
     return result
 }
